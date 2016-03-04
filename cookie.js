693d4b22cc1aee1953c5fd2ccedee91990c424fd
hanjie <jie_han@uyoung.com.cn>
2016-03-04 15:27:38 +0800

    fix

--- cookie.js
+++ cookie.js
@@ -1,3 +1,15 @@
 function alertMsg(msg){
 	alert(msg);
 }
+
+function setCookie(data){
+	document.cookie = data.key + "=" + data.val;
+}
+
+function getCookie(key){
+	var arr,reg=new RegExp("(^| )"+key+"=([^;]*)(;|$)");
+	if(arr=document.cookie.match(reg))
+	return unescape(arr[2]);
+	else
+	return null;
+}
